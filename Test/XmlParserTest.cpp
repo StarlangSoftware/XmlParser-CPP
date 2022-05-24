e3d778a625f926cc0b0f66027ac13d8a62d88c4f
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-05-24 10:55:44 +0300

    Added Test files and upgraded to C++20.

---
+++ Test/XmlParserTest.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Olcay Taner YILDIZ on 24.05.2022.
+//
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include "../XmlDocument.h"
+
+TEST_CASE("XmlParserTest") {
+    auto* doc = new XmlDocument("test.xml");
+    doc->parse();
+    XmlElement* root = doc->getFirstChild();
+    REQUIRE("frameset" == root->getName());
+    XmlElement* firstChild = root->getFirstChild();
+    REQUIRE("role" == firstChild->getName());
+    REQUIRE("ali veli \"deneme yapmak\" = anlamında > bir deyim" == firstChild->getPcData());
+    XmlElement* secondChild = firstChild->getNextSibling();
+    REQUIRE("perceiver, alien \"x3\" to whom?" == secondChild->getAttributeValue("descr"));
+    REQUIRE("PAG" == secondChild->getAttributeValue("f"));
+    REQUIRE("2" == secondChild->getAttributeValue("n"));
+}
