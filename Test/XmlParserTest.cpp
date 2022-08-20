40244c88139de4a52318c3836e5b61c7df320867
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-08-20 13:54:27 +0300

    Added Conan packaging.

--- Test/XmlParserTest.cpp
+++ Test/XmlParserTest.cpp
@@ -4,7 +4,7 @@
 
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include "../XmlDocument.h"
+#include "../src/XmlDocument.h"
 
 TEST_CASE("XmlParserTest") {
     auto* doc = new XmlDocument("test.xml");
